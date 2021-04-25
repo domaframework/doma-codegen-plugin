47c545111794ae387ccf9d5cf234b3153f58f47a
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-04-25 09:54:29 +0900

    Disable up-to-date validation checks (#60)

--- codegen/src/main/java/org/seasar/doma/gradle/codegen/task/CodeGenDtoTask.java
+++ codegen/src/main/java/org/seasar/doma/gradle/codegen/task/CodeGenDtoTask.java
@@ -9,7 +9,7 @@ import org.gradle.api.provider.Property;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Nested;
-import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.options.Option;
 import org.seasar.doma.gradle.codegen.GlobalFactory;
@@ -127,7 +127,7 @@ public class CodeGenDtoTask extends DefaultTask {
     return languageClassResolver;
   }
 
-  @OutputDirectory
+  @Optional
   public DirectoryProperty getSourceDir() {
     return sourceDir;
   }
