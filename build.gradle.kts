761c91dccdc4287b9902881c580ebace02c0912d
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2021-05-08 00:45:38 +0900

    Make tests runnable from IntelliJ IDEA (#67)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    base
+    java
     id("net.researchgate.release") version "2.8.1"
 }
 
 configure<net.researchgate.release.ReleaseExtension> {
     newVersionCommitMessage = "[Gradle Release Plugin] - [skip ci] new version commit: "
     tagTemplate = "v\$version"
+}
+
+allprojects {
+    apply(plugin = "java")
+    tasks {
+        test {
+            useJUnitPlatform()
+        }
+    }
 }
